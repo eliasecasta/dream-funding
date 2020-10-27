2b21683d76c039b90c28ac7876e9a1d2ce0279c6
Elias Castañeda <e71az@icloud.com>
2020-10-27 09:28:52 -0700

    Added sidebar button functionality

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -11,7 +11,6 @@ require("channels")
 import 'bootstrap'
 import 'css/styles'
 
-
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
 // or the `imagePath` JavaScript helper below.
---
+++ app/javascript/packs/sidebar.js
@@ -0,0 +1,15 @@
+// document.getElementById("openBtn").addEventListener("click", () => openNav());
+// document.getElementById("closeBtn").addEventListener("click", () => closeNav());
+
+/* Set the width of the sidebar to 250px and the left margin of the page content to 250px */
+function openNav() {
+  document.getElementById("mySidebar").style.width = "250px";
+  document.getElementById("main").style.marginLeft = "250px";
+}
+window.openNav = openNav
+/* Set the width of the sidebar to 0 and the left margin of the page content to 0 */
+function closeNav() {
+  document.getElementById("mySidebar").style.width = "0";
+  document.getElementById("main").style.marginLeft = "0";
+}
+window.closeNav = closeNav
