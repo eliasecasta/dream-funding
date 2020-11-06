aeacaf86764b9fbe2e36615de7b18bccc30ecd80
Elias Castañeda <e71az@icloud.com>
2020-11-05 22:47:09 -0700

    Created helpers for dream show and index views

--- app/helpers/dreams_helper.rb
+++ app/helpers/dreams_helper.rb
@@ -1,6 +1,14 @@
 module DreamsHelper
+
   def percentage_collected(dream)
     (dream.donations.dreams_sum_amount(dream.id).to_f / dream.money_objective) * 100
   end
 
+  def has_image(dream)
+    image_tag(dream.image.attached? ? dream.image : "no_image.png")
+  end
+
+  def has_avatar(donation)
+    image_tag(donation.avatar.attached? ? donation.avatar : "saitama.jpg")
+  end
 end
