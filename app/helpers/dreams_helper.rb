ad924f7ad492b3b5cf9202564e9ac80571a1761e
Elias Castañeda <e71az@icloud.com>
2020-11-05 23:11:18 -0700

    Fixed rubocop errors

--- app/helpers/dreams_helper.rb
+++ app/helpers/dreams_helper.rb
@@ -1,14 +1,13 @@
 module DreamsHelper
-
   def percentage_collected(dream)
     (dream.donations.dreams_sum_amount(dream.id).to_f / dream.money_objective) * 100
   end
 
-  def has_image(dream)
-    image_tag(dream.image.attached? ? dream.image : "no_image.png")
+  def image?(dream)
+    image_tag(dream.image.attached? ? dream.image : 'no_image.png')
   end
 
-  def has_avatar(donation)
-    image_tag(donation.avatar.attached? ? donation.avatar : "saitama.jpg")
+  def avatar?(donation)
+    image_tag(donation.avatar.attached? ? donation.avatar : 'saitama.jpg')
   end
 end
